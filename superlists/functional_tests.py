7a563afc121c2e67fb6b2da398470a78f3e0a625
xiaoheizhou <1667058312@qq.com>
2018-05-06 21:34:45 +0800

    使用注释编写规格的首个功能测试，而且使用了unittest

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -1,6 +1,23 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Chrome()
-browser.get('http://localhost:8000')
 
-assert 'Django' in browser.title
+class NewVisitorTest(unittest.TestCase):
+    def setUp(self):
+        '''打开浏览器并且使浏览器停留3秒'''
+        self.brower = webdriver.Chrome()
+        self.brower.implicitly_wait(3)
+
+    def tearDown(self):
+        '''关闭浏览器'''
+        self.brower.quit()
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        '''打开Django首页，在title中找To-Do字段，最后结束测试'''
+        self.brower.get('http://localhost:8000')
+        self.assertIn('To-Do',self.brower.title)
+        self.fail('Finish the test!')
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
+
